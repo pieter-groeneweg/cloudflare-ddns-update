d7a462c67192dcb5338d04870e24d1dbed18f50a
pieter-groeneweg <pieter@groenewegdigitaal.nl>
2017-01-10 15:41:52 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('SECURITY') or exit();
+define('DOMAIN', 'domain.com');
+define('X_AUTH_EMAIL', 'email_you_use_to_login@domain.com');
+define('X_AUTH_KEY', 'your_auth_key_from_cloudflare');
+define('SUBDOMAINS', array('subdomain_one.domain.com','subdomain_two.domain.com'));
+
+?>
